080caae6a945b36669190f1b51e303c432e783c5
Mantaro <manta.xylish@gmail.com>
2017-10-31 15:57:35 +0900

    add ginza line and integrated data with JR and metro

--- handson/station_data/make_json.py
+++ handson/station_data/make_json.py
@@ -1,14 +1,32 @@
+import os
 import json
 
-stations_json = {}
+output = {}
+lines_dir = './original_line_data'
 
-with open('11302.json', 'r') as f:
+# add stations on all lines
+for line_file in os.listdir(lines_dir):
+    with open(os.path.join(lines_dir, line_file), 'r') as f:
+        print(os.path.join(lines_dir, line_file))
+        json_dict = json.load(f)
+        line = json_dict['line_name']
+        stations = json_dict['station_l']
+
+        tmp = {}
+        for station in stations:
+            tmp[station['station_name']] = [station['lon'], station['lat'], station['station_name']]
+        output[line] = tmp
+
+print(output)
+
+# add stations on one line
+"""
+with open(os.path.join(lines_dir, '28001.json'), 'r') as f:
     json_dict = json.load(f)
     stations = json_dict["station_l"]
     for station in stations:
-        stations_json[station['station_name']] = [station['lon'], station['lat'], station['station_name']]
+        output[station['station_name']] = [station['lon'], station['lat'], station['station_name']]
+"""
 
-print(stations_json)
-
-with open('test.json', 'w') as f:
-    json.dump(stations_json, f)
+# with open('test.json', 'w') as f:
+#     json.dump(stations_json, f)
